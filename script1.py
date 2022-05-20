21114a7b581a7d1204ba3cb8f90d769db059aeeb
EvanRobertsn <evan.robertson@flemingcollege.ca>
2022-05-19 23:44:52 -0400

    Added comment to script1.py

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
+#Print something witty and nuanced that will inspire great awe within the reader, possibly bringing them to tears
 print("Simple is better than complex.\n")
 print("Complex is better than complicated.\n")
