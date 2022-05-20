614909263534e12e5423979748f8438ec4b0a1f8
EvanRobertsn <evan.robertson@flemingcollege.ca>
2022-05-19 23:59:40 -0400

    Modded both files in repo Clone
    -Removed print in script1.py
    -Added print in script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 #Print something witty and nuanced that will inspire great awe within the reader, possibly bringing them to tears
 print("Simple is better than complex.\n")
-print("Complex is better than complicated.\n")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 #This comment functions as a comment
 print("Now is better than never.\n")
 print("Although never is often better than *right* now.\n")
+print("I yearn for the day python will create a built in function for multi-line comments")
