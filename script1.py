88d59b98cf64581d296f8209f8c2973643764b28
EvanRobertsn <evan.robertson@flemingcollege.ca>
2022-05-20 00:06:28 -0400

    Added Micro Transactions feature
    Created new and innovative feature for text based software

--- script1.py
+++ script1.py
@@ -1,2 +1,4 @@
 #Print something witty and nuanced that will inspire great awe within the reader, possibly bringing them to tears
 print("Simple is better than complex.\n")
+
+#Pay $1.99 to view this comment and bask in the amazing new feature
